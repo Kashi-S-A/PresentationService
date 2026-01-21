9be2125527d4fbf05dd964717e163dbfb19383d7
Shani-J11 <shanijaiswar14@gmail.com>
2026-01-21 20:39:24 +0530

    updated presentation service logic

--- src/main/java/com/presentation/repository/PresentationRepo.java
+++ src/main/java/com/presentation/repository/PresentationRepo.java
@@ -1,9 +1,13 @@
 package com.presentation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.presentation.entity.Presentation;
 
 public interface PresentationRepo extends JpaRepository<Presentation, Integer> {
+
+	 List<Presentation> findByUserId(Integer userId);
 
 }
