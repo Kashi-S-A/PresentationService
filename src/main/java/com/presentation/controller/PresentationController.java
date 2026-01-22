6daaf96d39f221384e46bdbf8ad952b0b5518e97
alpha-knowledge <alphaknowledgeofficial@gmail.com>
2026-01-22 11:14:42 +0530

    Status and Score Controller

--- src/main/java/com/presentation/controller/PresentationController.java
+++ src/main/java/com/presentation/controller/PresentationController.java
@@ -1,10 +1,40 @@
 package com.presentation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.presentation.entity.Presentation;
+import com.presentation.enums.PresentationStatus;
+import com.presentation.service.PresentationService;
 
 @RestController
 @RequestMapping("/presentation")
 public class PresentationController {
+	
+	@Autowired
+	private PresentationService presentationService;
+
+	@PutMapping("/change-status/{id}")
+	public ResponseEntity<Presentation> changeStatus(
+			@PathVariable Integer id,
+			@RequestParam PresentationStatus status) {
+
+		return ResponseEntity.ok(
+				presentationService.changeStatus(id, status));
+	}
+
+	@PutMapping("/update-total-score/{id}")
+	public ResponseEntity<Presentation> updateTotalScore(
+			@PathVariable Integer id,
+			@RequestParam Double totalScore) {
+
+		return ResponseEntity.ok(
+				presentationService.updateUserTotalScore(id, totalScore));
+	}
 
 }
--- src/main/java/com/presentation/service/PresentationService.java
+++ src/main/java/com/presentation/service/PresentationService.java
@@ -1,5 +1,13 @@
 package com.presentation.service;
 
+import com.presentation.entity.Presentation;
+import com.presentation.enums.PresentationStatus;
+
 public interface PresentationService {
+
+	Presentation changeStatus(Integer pid, PresentationStatus status);
+
+	Presentation updateUserTotalScore(Integer pid, Double userTotalScore);
+	
 
 }
